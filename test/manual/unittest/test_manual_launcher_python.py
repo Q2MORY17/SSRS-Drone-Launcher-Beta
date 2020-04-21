63ad88ae8fe875e5af30760674537807bd49619a
ilarisilander <ilarisilander@hotmail.com>
2020-04-21 18:29:30 +0200

    SSRS2-49 Add test encoders ready

--- test/manual/unittest/test_manual_launcher_python.py
+++ test/manual/unittest/test_manual_launcher_python.py
@@ -4,7 +4,7 @@ import sys
 import flask
 import pytest
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/../python")
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/../../python")
 
 from unittest.mock import MagicMock
 
