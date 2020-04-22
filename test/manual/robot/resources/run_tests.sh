f73d615cb57656d8a80cca0542f00d981ffc98f1
vadis <d@jdd.nu>
2020-04-22 14:47:59 +0200

    Testing on windows

--- test/manual/robot/resources/run_tests.sh
+++ test/manual/robot/resources/run_tests.sh
@@ -5,12 +5,15 @@ DRONE_IP=$(python ./library/getip.py)
 python ../../../python/dronelauncher_python.py &> /dev/null &
 DRONE_PID=$!
 
-if [ $# -eq 0 ]; then
-    robot -d results *.robot
-else
-    robot -d results $@
-fi
 
+# if [ $# -eq 0 ]; then
+#     robot -d results *.robot
+# else
+#     robot -d results $@
+# fi
+
+echo $DRONE_IP
+echo $DRONE_PID
 kill $DRONE_PID
 
 exit 0
