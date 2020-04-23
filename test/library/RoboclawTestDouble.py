c4b592cec7401b6cb5c37eee43b86cbfe86d6764
Viktor Nilsson <viktor_741@hotmail.com>
2020-04-23 18:43:37 +0200

    SRS2-5 - Remove auto start of virtual com port

--- test/library/RoboclawTestDouble.py
+++ test/library/RoboclawTestDouble.py
@@ -8,13 +8,7 @@ import psutil
 class RoboclawTestDouble:
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
 
-    com_port_app = "setupg.exe"
-    com_port_app_path = "C:\\Program Files (x86)\\com0com\\"
-
     def __init__(self):
-        if self.com_port_app in (p.name() for p in psutil.process_iter()):
-            if not subprocess.Popen(self.com_port_app_path + self.com_port_app, shell=True):
-                raise FileNotFoundError(self.com_port_app_path)
 
     @keyword
     def run(self):
