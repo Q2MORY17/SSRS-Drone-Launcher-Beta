091da801d858dc0e620ef18e837931f7a594932d
Viktor Nilsson <viktor_741@hotmail.com>
2020-04-23 20:10:02 +0200

    SSRS2-5 Fix not being able to catch multiple roboclaw commands

--- test/library/RoboclawTestDouble.py
+++ test/library/RoboclawTestDouble.py
@@ -1,5 +1,4 @@
 import serial
-import subprocess
 
 
 class RoboclawTestDouble:
@@ -18,20 +17,28 @@ class RoboclawTestDouble:
 
     com_port = "COM5"
 
+    # Need a 1s timeout to be able to handle multiple commands being sent after each other
+    timeout = 1
+
     def __init__(self):
         try:
-            self.virtual_com_port = serial.Serial(self.com_port, timeout=0.5)
+            self.virtual_com_port = serial.Serial(self.com_port, timeout=self.timeout)
         except:
             raise SystemError("Could not connect to " + self.com_port)
 
     def read_roboclaw_command(self):
         command = self.virtual_com_port.readline()
+        complete_command = b''
 
         if not command:
             raise AssertionError("No command was sent to the roboclaw")
+        else:
+            while command:
+                complete_command = complete_command + command
+                command = self.virtual_com_port.read_until()
 
-        print(command)
-        return
+        print(complete_command)
+        return complete_command
 
 
 if __name__ == "__main__":
