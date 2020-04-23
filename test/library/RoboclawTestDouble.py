9b18084f3a99f982d3ce3eb34c7378ba8850cef8
Viktor Nilsson <viktor_741@hotmail.com>
2020-04-23 19:07:35 +0200

    SSRS2-5 Add keyword to read roboclaw command

--- test/library/RoboclawTestDouble.py
+++ test/library/RoboclawTestDouble.py
@@ -1,18 +1,26 @@
 import serial
 import subprocess
-#import dronelauncher_python
-from robot.api.deco import keyword
-import psutil
 
 
 class RoboclawTestDouble:
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
 
-    def __init__(self):
+    com_port = "COM5"
 
-    @keyword
-    def run(self):
-        print("PASS")
+    def __init__(self):
+        try:
+            self.virtual_com_port = serial.Serial(self.com_port, timeout=0.5)
+        except:
+            raise SystemError("Could not connect to " + self.com_port)
+
+    def read_roboclaw_command(self):
+        command = self.virtual_com_port.readline()
+
+        if not command:
+            raise AssertionError("No command was sent to the roboclaw")
+
+        print(command)
+        return
 
 
 if __name__ == "__main__":
