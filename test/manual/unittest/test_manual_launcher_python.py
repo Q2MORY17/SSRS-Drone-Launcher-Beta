3a20f3bf62f94f67d713d9bbf1ca5a6b8f7b9020
mihaelaiovu <iovu.mihaela@hotmail.com>
2020-04-27 10:22:58 +0200

    Added code to check the values in SpeedDistanceM2

--- test/manual/unittest/test_manual_launcher_python.py
+++ test/manual/unittest/test_manual_launcher_python.py
@@ -6,7 +6,7 @@ import pytest
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/../../python")
 
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, call
 
 import python.dronelauncher_python
 
@@ -39,9 +39,11 @@ def test_function_launch_backwards_with_valid_speed(dl):
 def test_function_launch_backwards_with_invalid_speed(dl):
     # GIVEN
     dl.rc = MagicMock()
+
     # WHEN
     dl.launch_speed_manual = 30000
     returnValue = dl.function_launch_backwards()
+
     # THEN
     dl.rc.BackwardM2.assert_called_with(dl.address_2, 30000)
     assert returnValue != ('', 204)
@@ -63,9 +65,11 @@ def test_function_launch_forwards_with_valid_speed(dl):
 def test_function_launch_forwards_with_invalid_speed(dl):
     # GIVEN
     dl.rc = MagicMock()
+
     # WHEN
     dl.launch_speed_manual = 999
     returnValue = dl.function_launch_forwards()
+
     # THEN
     dl.rc.ForwardM2.assert_called_with(dl.address_2, 999)
     assert returnValue != ('', 204)
@@ -74,8 +78,10 @@ def test_function_launch_forwards_with_invalid_speed(dl):
 def test_function_launch_stop(dl):
     # GIVEN
     dl.rc.ForwardM2 = MagicMock(return_value=True)
+
     # WHEN
     returnValue = dl.function_launch_stop()
+
     # THEN
     dl.rc.ForwardM2.assert_called_with(dl.address_2, 0)
     assert returnValue == ('', 204)
@@ -84,8 +90,10 @@ def test_function_launch_stop(dl):
 def test_function_launch_position_encoders_not_ready(dl):
     # GIVEN
     dl.encoders_ready = 0
+
     # WHEN
     returnValue = dl.function_launch_position()
+
     # THEN
     assert returnValue == ('', 403)
 
@@ -98,18 +106,20 @@ def test_function_launch_position_encoders_ready_with_invalid_value(dl, invalid_
     # Creates a flask request context
     # GIVEN
     dl.encoders_ready = 1
+
     # WHEN
     with app.test_request_context('/app_launch_position', data={'launch_position': invalid_data}):
         # THEN
         assert dl.function_launch_position() == ('', 400)
 
+
 def test_function_launch_position_encoders_ready_launch_position_zero(dl):
     # GIVEN
     dl.encoders_ready = 1
     app_client = dl.app.test_client()
-    dl.rc.ReadEncM2 = MagicMock(return_value=(1, 2, 2))
-    dl.rc.SpeedDistanceM2 = MagicMock()
-    dl.rc.ReadBuffers = MagicMock(return_value=(0, 0, 0x80))
+    dl.rc = MagicMock()
+    dl.rc.ReadEncM2.return_value = (1, 2, 2)
+    dl.rc.ReadBuffers.return_value = (0, 0, 0x80)
 
     # WHEN
     response = app_client.post('/app_launch_position', content_type='multipart/form-data',
@@ -117,15 +127,21 @@ def test_function_launch_position_encoders_ready_launch_position_zero(dl):
 
     # THEN
     assert response.status_code == 204
+    calls = [call(129, -2500, 2, 1),
+             call(129, 0, 0, 0),
+             call(129, 2500, 2188, 0),
+             call(129, 0, 0, 0)]
+    dl.rc.SpeedDistanceM2.assert_has_calls(calls)
+    assert dl.rc.SpeedDistanceM2.call_count == 4
 
 
 def test_function_launch_position_encoders_ready_launch_position_higher_than_zero(dl):
     # GIVEN
     dl.encoders_ready = 1
     app_client = dl.app.test_client()
-    dl.rc.ReadEncM2 = MagicMock(return_value=(1, 4, 2))  # launch_actual = 4 ,  launch_increment = -4
-    dl.rc.SpeedDistanceM2 = MagicMock()
-    dl.rc.ReadBuffers = MagicMock(return_value=(0, 0, 0x80))
+    dl.rc = MagicMock()
+    dl.rc.ReadEncM2.return_value = (1, 4, 2)  # launch_actual = 4 ,  launch_increment = -4
+    dl.rc.ReadBuffers.return_value = (0, 0, 0x80)
 
     # WHEN
     response = app_client.post('/app_launch_position', content_type='multipart/form-data',
@@ -133,15 +149,21 @@ def test_function_launch_position_encoders_ready_launch_position_higher_than_zer
 
     # THEN
     assert response.status_code == 204
+    calls = [call(129, -2500, 4, 1),
+             call(129, 0, 0, 0),
+             call(129, 2500, 3519, 0),
+             call(129, 0, 0, 0)]
+    dl.rc.SpeedDistanceM2.assert_has_calls(calls)
+    assert dl.rc.SpeedDistanceM2.call_count == 4
 
 
 def test_function_launch_position_encoders_ready_launch_position_max(dl):
     # GIVEN
     dl.encoders_ready = 1
     app_client = dl.app.test_client()
-    dl.rc.ReadEncM2 = MagicMock(return_value=(1, -1.5, 2))
-    dl.rc.SpeedDistanceM2 = MagicMock()
-    dl.rc.ReadBuffers = MagicMock(return_value=(0, 0, 0x80))
+    dl.rc = MagicMock()
+    dl.rc.ReadEncM2.return_value = (1, -1.5, 2)
+    dl.rc.ReadBuffers.return_value = (0, 0, 0x80)
 
     # WHEN
     response = app_client.post('/app_launch_position', content_type='multipart/form-data',
@@ -149,4 +171,9 @@ def test_function_launch_position_encoders_ready_launch_position_max(dl):
 
     # THEN
     assert response.status_code == 204
-
+    calls = [call(129, 2500, 1.5, 1),
+             call(129, 0, 0, 0),
+             call(129, 2500, 16991.5, 0),
+             call(129, 0, 0, 0)]
+    dl.rc.SpeedDistanceM2.assert_has_calls(calls)
+    assert dl.rc.SpeedDistanceM2.call_count == 4
