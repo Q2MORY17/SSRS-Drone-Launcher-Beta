053a749d65f602e8b45910fce6816fd7b295c2f9
elinarnten <elin.arnten@hotmail.com>
2020-04-28 14:59:03 +0200

    SSRS3-55 reset encoders test

--- test/settings/unittest/test_dronelauncher_python.py
+++ test/settings/unittest/test_dronelauncher_python.py
@@ -1,11 +1,27 @@
-import python.dronelauncher_python
+import os
+import sys
 import pytest
+import python.dronelauncher_python
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/../../python")
 
 
-@pytest.fixture
+@pytest.fixture()
 def dl():
     dl = python.dronelauncher_python
     return dl
 
 def test_comports_closed(dl):
     assert 0 == dl.rc.Open()
+
+def test_function_reset_encoders(dl):
+    """Line 440"""
+
+    #GIVEN
+    dl.encoders_ready = 1
+
+    #THEN
+    returnValue = dl.function_reset_encoders()
+
+    #WHEN
+    assert returnValue == (''), 204
