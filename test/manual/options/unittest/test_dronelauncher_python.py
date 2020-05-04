a6ee9788a191727e7ac44ff9315ddc876f706b95
elinarnten <elin.arnten@hotmail.com>
2020-05-04 14:18:09 +0200

    SSRS3-72 fixed the folder and changed the search path

--- test/manual/options/unittest/test_dronelauncher_python.py
+++ test/manual/options/unittest/test_dronelauncher_python.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/../../python")
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/../../../python")
 
 #from unittest.mock import Mock
 #mock = Mock()
