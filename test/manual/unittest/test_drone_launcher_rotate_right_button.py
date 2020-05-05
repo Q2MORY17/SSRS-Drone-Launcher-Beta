6d2d5fb3a14243314e5ca3523d44d82552fbf72b
Bobi Andreevski <bob.andreevski@gmail.com>
2020-05-05 21:06:46 +0200

    SSRS1-126 Added pytest file test_drone_launch_rotate_right_button

---
+++ test/manual/unittest/test_drone_launcher_rotate_right_button.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+import flask
+import pytest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/../../python")
+
+from unittest.mock import MagicMock, call
+
+import python.dronelauncher_python
+app = flask.Flask(__name__)
+
+
+@pytest.fixture()
+def dl():
+    print('\n*********Start*********')
+    dl = python.dronelauncher_python
+    yield dl
+    print('\n**********End**********')
+
+
+def test_function_rotate_right(dl):
+    # GIVEN
+    dl.rc.ForwardM2 = MagicMock(return_value=True)
+
+
+    # WHEN
+    returnValue = dl.function_rotation_position()
+
+    # THEN
+    dl.rc.ForwardM2.assert_called_with(dl.address, dl.rotation_speed_manual)
+     assert returnValue == ('', 204)
