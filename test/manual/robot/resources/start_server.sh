8889e387821f11f015cb0b49d63a52f8db494e38
vadis <d@jdd.nu>
2020-05-07 18:07:56 +0200

    SSRS1-129 Added checks for server response in pitch value test

--- test/manual/robot/resources/start_server.sh
+++ test/manual/robot/resources/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 ../../../python/dronelauncher_python.py &> .dronelauncher.log
+python ../../../python/dronelauncher_python.py &> >(sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g") &> .dronelauncher.log
 exit 0
