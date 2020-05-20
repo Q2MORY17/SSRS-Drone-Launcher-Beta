d0562a5203a7cff2862abb16ada9017ec9471846
Saba Mustfa <sabamustafaali@yahoo.com>
2020-05-20 13:12:06 +0200

    SSRS1-127 rechecked all the unit tests

--- test/manual/unittest/test_drone_launcher_MaxMin.py
+++ test/manual/unittest/test_drone_launcher_MaxMin.py
@@ -20,17 +20,16 @@ def test_function_max_pitch(self):
     self.encoders_ready = 1
     app_client = self.app.test_client()
     self.rc = MagicMock()
-    self.rc.ReadEncM1.return_value = (7, -8, 1)
+    self.rc.ReadEncM1.return_value = (1, -1.5, 2)
+    self.rc.ReadBuffers.return_value = (0, 0, 0x80)
 
     # WHEN
     response = app_client.post('/app_max_pitch', content_type='multipart/form-data',
-                               data={'app_max_pitch': '250'})
+                               data={'pitch_position': '90'})
 
     # THEN
     assert response.status_code == 204
-    calls = [call(128, 7000, 355008, 1),
-             call(128, 0, 0, 0)]
-
+    calls = [call(128, 7000, 355001.5, 1), call(128, 0, 0, 0)]
     self.rc.SpeedDistanceM1.assert_has_calls(calls)
     assert self.rc.SpeedDistanceM1.call_count == 2
 
@@ -40,17 +39,16 @@ def test_function_min_pitch(self):
     self.encoders_ready = 1
     app_client = self.app.test_client()
     self.rc = MagicMock()
-    self.rc.ReadEncM1.return_value = (7, -8, 1)
+    self.rc.ReadEncM1.return_value = (1, 3, 2)
+    self.rc.ReadBuffers.return_value = (0, 0, 0x80)
 
     # WHEN
     response = app_client.post('/app_min_pitch', content_type='multipart/form-data',
-                               data={'app_min_pitch': '320'})
+                               data={'pitch_position': '111'})
 
     # THEN
     assert response.status_code == 204
-    calls = [call(128, 7000, 8, 1),
-             call(128, 0, 0, 0)]
-
+    calls = [call(128, -7000, 3, 1), call(128, 0, 0, 0)]
     self.rc.SpeedDistanceM1.assert_has_calls(calls)
     assert self.rc.SpeedDistanceM1.call_count == 2
 
@@ -60,17 +58,16 @@ def test_function_max_lift(self):
     self.encoders_ready = 1
     app_client = self.app.test_client()
     self.rc = MagicMock()
-    self.rc.ReadEncM1.return_value = (7, -8, 1)
+    self.rc.ReadEncM1.return_value = (2, 7, 3)
+    self.rc.ReadBuffers.return_value = (0, 0, 0x81)
 
     # WHEN
     response = app_client.post('/app_max_lift', content_type='multipart/form-data',
-                               data={'app_max_lift': '450'})
+                               data={'lift_position': '450'})
 
     # THEN
     assert response.status_code == 204
-    calls = [call(129, 420, 19008, 1),
-             call(129, 0, 0, 0)]
-
+    calls = [call(129, 420, 18993, 1), call(129, 0, 0, 0)]
     self.rc.SpeedDistanceM1.assert_has_calls(calls)
     assert self.rc.SpeedDistanceM1.call_count == 2
 
@@ -81,32 +78,20 @@ def test_function_min_lift(self):
     app_client = self.app.test_client()
     self.rc = MagicMock()
     self.rc.ReadEncM1.return_value = (7, -8, 1)
+    self.rc.ReadBuffers.return_value = (0, 0, 0x81)
 
     # WHEN
     response = app_client.post('/app_min_lift', content_type='multipart/form-data',
-                               data={'app_min_lift': '330'})
+                               data={'lift_position': '80'})
 
     # THEN
     assert response.status_code == 204
-    calls = [call(129, 420, 8, 1),
-             call(129, 0, 0, 0)]
-
+    calls = [call(129, 420, 8, 1), call(129, 0, 0, 0)]
     self.rc.SpeedDistanceM1.assert_has_calls(calls)
     assert self.rc.SpeedDistanceM1.call_count == 2
 
 
-def test_function_max_pitch_encoders_not_ready(self):
-    # GIVEN
-    self.encoders_ready = 0
-
-    # WHEN
-    returnValue = self.function_max_pitch()
-
-    # THEN
-    assert returnValue == ('', 403)
-
-
-def test_function_min_pitch_encoders_not_ready(self):
+def test_function_pitch_position_encoders_not_ready(self):
     # GIVEN
     self.encoders_ready = 0
 
@@ -117,7 +102,7 @@ def test_function_min_pitch_encoders_not_ready(self):
     assert returnValue == ('', 403)
 
 
-def test_function_max_lift_encoders_not_ready(self):
+def test_function_lift_position_encoders_not_ready(self):
     # GIVEN
     self.encoders_ready = 0
 
@@ -128,14 +113,6 @@ def test_function_max_lift_encoders_not_ready(self):
     assert returnValue == ('', 403)
 
 
-def test_function_min_lift_encoders_not_ready(self):
-    # GIVEN
-    self.encoders_ready = 0
 
-    # WHEN
-    returnValue = self.function_min_lift()
-
-    # THEN
-    assert returnValue == ('', 403)
 
 
